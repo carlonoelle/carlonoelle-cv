42db9c7e8ccd8eb93693de8c009a6039df930ed7
Carlo Nölle <cno@timetoact.de>
2023-08-09 00:42:08 +0200

    add links

--- src/data/contact.json.ts
+++ src/data/contact.json.ts
@@ -6,8 +6,18 @@ export interface IContactTemplate {
 
 export const contact: IContactTemplate[] = [
   {
+    link: 'https://www.linkedin.com/in/carlonoelle/',
+    type: 'LinkedIn',
+    title: 'carlonoelle',
+  },
+  {
     link: 'mailto:hello@carlonoelle.de',
     type: 'Email',
     title: 'hello@carlonoelle.de',
+  },
+  {
+    link: 'https://www.xing.com/profile/Carlo_Noelle',
+    type: 'Xing',
+    title: 'Carlo_Noelle',
   },
 ];
